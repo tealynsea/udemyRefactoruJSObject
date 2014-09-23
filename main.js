9826ef70603fd38f7f839745a757b95e89d346b2
Teri Castle <tea.@gmail.com>
2014-09-23 13:06:10 -0600

    exercises on objects

---
+++ main.js
@@ -0,0 +1,61 @@
+// Lecture 6 - Objects
+// var myObject = {
+// 	name: "Chris",
+// 	language: "JavaScript",
+// 	codingLevel: 9001,
+// 	favoriteGames: ['Braid', 'Half-Life'],
+// 	codingStack: {
+// 		frontEnd: 'JavaScript',
+// 		backEnd: 'Node.js'
+// 	}
+// };
+
+// console.log( myObject );
+
+// Lecture 7 - For In Loop
+// var myObject = {
+// 	name: "Chris",
+// 	language: "JavaScript",
+// 	codingLevel: 9001,
+// 	favoriteGames: ['Braid', 'Half-Life'],
+// 	codingStack: {
+// 		frontEnd: 'JavaScript',
+// 		backEnd: 'Node.js'
+// 	}
+// };
+
+// for(var prop in myObject){
+// 	console.log( prop )
+// }
+
+// Lecture 8 Dot v. Brackets []
+// var myObject = {
+// 	name: "Chris",
+// 	language: "JavaScript",
+// 	codingLevel: 9001,
+// 	favoriteGames: ['Braid', 'Half-Life'],
+// 	codingStack: {
+// 		frontEnd: 'JavaScript',
+// 		backEnd: 'Node.js'
+// 	}
+// };
+
+// for(var prop in myObject){
+// 	console.log( prop, myObject[prop] );
+// }
+
+/* in console:
+	myObject.language output "JavaScript"
+	myObject["language"] output "JavaScript"
+	var keyToGet = "codingLevel";
+	myObject[keyToGet];
+	myObject.name + myObject["language"]*/
+
+
+
+
+
+
+
+
+
